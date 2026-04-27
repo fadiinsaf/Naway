05ac80b67f3d8f56c9ef62d0bce31ebbb49e4a29
fadiinsaf <fadi_man_2005@outlook.fr>
2026-04-27 16:15:02 +0100

    feat: create polymorphic comments table migration

--- database/migrations/2026_03_25_115618_create_comments_table.php
+++ database/migrations/2026_03_25_115618_create_comments_table.php
@@ -10,6 +10,7 @@ return new class extends Migration {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->text('content');
+            $table->boolean('approved')->default(false);
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
 
             $table->unsignedBigInteger('entity_id');
