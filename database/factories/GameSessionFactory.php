663f2016f133791ed6fdce0015e093395531ca42
fadiinsaf <fadi_man_2005@outlook.fr>
2026-04-27 16:39:42 +0100

    chore: add GameSessionFactory to generate test game sessions

---
+++ database/factories/GameSessionFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\GameSession;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\GameSession>
+ */
+class GameSessionFactory extends Factory
+{
+    protected $model = GameSession::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'score' => $this->faker->numberBetween(0, 100),
+            'current_level' => $this->faker->numberBetween(1, 10),
+            'started_at' => $this->faker->dateTimeThisMonth(),
+            'ended_at' => $this->faker->dateTimeThisMonth(),
+        ];
+    }
+}
