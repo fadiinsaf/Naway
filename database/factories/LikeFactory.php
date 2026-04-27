5527c88102442f0bfc50f3a0129e36baa547fd9a
fadiinsaf <fadi_man_2005@outlook.fr>
2026-04-27 16:31:04 +0100

    chore: add factory LikeFactory.php

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\Like;
+use App\Models\User;
+use App\Models\Comment;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Like>
+ */
+class LikeFactory extends Factory
+{
+    protected $model = Like::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'likeable_id' => Comment::factory(),
+            'likeable_type' => Comment::class,
+        ];
+    }
+}
