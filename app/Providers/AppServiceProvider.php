16a7cae24d9daa84d300389f4bfffe82172ee0f0
fadiinsaf <fadi_man_2005@outlook.fr>
2026-04-29 01:20:41 +0100

    chore: update AppServiceProvider to force https files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,17 +4,22 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
-    
+
     public function register(): void
     {
 
     }
 
-    public function boot(): void
+    public function boot()
     {
+        if (config('app.env') === 'production' || config('app.env') === 'staging') {
+            URL::forceScheme('https');
+        }
         Paginator::useTailwind();
     }
 }
